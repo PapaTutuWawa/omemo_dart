fa16f971132e7362976a864444ac034d996915f0
Alexander "PapaTutuWawa <papatutuwawa@polynom.me>
2022-08-08 19:11:27 +0200

    refactor: Move events to lib/src/omemo

--- lib/omemo_dart.dart
+++ lib/omemo_dart.dart
@@ -2,13 +2,13 @@ library omemo_dart;
 
 export 'src/double_ratchet/double_ratchet.dart';
 export 'src/errors.dart';
-export 'src/events.dart';
 export 'src/helpers.dart';
 export 'src/keys.dart';
 export 'src/omemo/bundle.dart';
 export 'src/omemo/device.dart';
 export 'src/omemo/encrypted_key.dart';
 export 'src/omemo/encryption_result.dart';
+export 'src/omemo/events.dart';
 export 'src/omemo/fingerprint.dart';
 export 'src/omemo/sessionmanager.dart';
 export 'src/trust/base.dart';
